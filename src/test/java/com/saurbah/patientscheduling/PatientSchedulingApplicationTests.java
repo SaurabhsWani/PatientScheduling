c34e9557adf8221ddca7c1d83a24946d0d8b44f0
SaurabhsWani <er.saurabhwani1@gmail.com>
2024-09-21 23:33:14 +0530

    Initial Setup

---
+++ src/test/java/com/saurbah/patientscheduling/PatientSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurbah.patientscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
